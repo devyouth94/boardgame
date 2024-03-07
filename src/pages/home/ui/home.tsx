a05ad414c1a6331ee191e83ba375609c473ff4d1
devyouth94 <devyouth94@gmail.com>
2024-03-07 16:34:57 +0900

    Feat: 초기 화면 설정

---
+++ src/pages/home/ui/home.tsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { useGetUserInfo } from "~/features/auth/service/auth.query";
+
+const Home = () => {
+  const navigate = useNavigate();
+
+  const { data: userInfo, isSuccess } = useGetUserInfo();
+
+  useEffect(() => {
+    if (!isSuccess) return;
+
+    if (!userInfo) {
+      navigate("/sign");
+    }
+
+    if (userInfo && userInfo["name_verified"]) {
+      navigate("/main");
+    } else {
+      navigate("/sign/setup-profile");
+    }
+  }, [isSuccess]);
+
+  return <div className="flex h-dvh items-center justify-center">index page</div>;
+};
+
+export default Home;
