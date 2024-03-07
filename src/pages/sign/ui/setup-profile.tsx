807e3dabe9569bd200c90ab50b9f851c5c2e3e67
devyouth94 <devyouth94@gmail.com>
2024-03-07 16:36:22 +0900

    Feat: 로그인 및 닉네임 설정 페이지 추가

---
+++ src/pages/sign/ui/setup-profile.tsx
@@ -0,0 +1,11 @@
+import SetupProfileForm from "~/features/auth/ui/setup-profile-form";
+
+const SetupProfile = () => {
+  return (
+    <main className="flex h-dvh items-center justify-center px-5">
+      <SetupProfileForm />
+    </main>
+  );
+};
+
+export default SetupProfile;
---
+++ src/pages/sign/ui/sign.tsx
@@ -0,0 +1,11 @@
+import SignInButtons from "~/features/auth/ui/sign-in-buttons";
+
+const Sign = () => {
+  return (
+    <main className="flex h-dvh items-center justify-center px-5">
+      <SignInButtons />
+    </main>
+  );
+};
+
+export default Sign;
